ccbc6ed51249243535716fe52b0fe0d60eb66e43
Aherreras3 <aherreras3@uteq.edu.ec>
2022-12-09 18:59:18 -0500

    Tarea Consumir ApiRestful

---
+++ app/src/main/java/com/example/apirestfulenformatojson/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.apirestfulenformatojson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.apirestfulenformatojson.WebService.Asynchtask;
+import com.example.apirestfulenformatojson.WebService.WebService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View viewLista){
+        Map<String,String> datos = new HashMap<String,String>();
+        WebService ws = new WebService("https://gorest.co.in/public/v1/users", datos, MainActivity.this,
+                MainActivity.this);
+        ws.execute("GET");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtmuestralist=findViewById(R.id.txtmuestralist);
+        String list="";
+        JSONObject objectojson=new JSONObject(result);
+        JSONArray JSONLista=objectojson.getJSONArray("data");
+        for(int i=0; i<JSONLista.length();i++){
+            JSONObject mostrarlista=JSONLista.getJSONObject(i);
+            list=list +"user:"+mostrarlista.getString("name").toString()+
+                    " "+mostrarlista.getString("email").toString()+"\n";
+
+        }
+        txtmuestralist.setText(list);
+
+    }
+}
